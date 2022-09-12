5d70793fa4bcbb65af11e818da77f3b3804f5899
portan17 <85976855+portan17@users.noreply.github.com>
2022-09-11 21:00:02 -0600

    ultimo
    tarea

--- desafio/src/Datos.jsx
+++ desafio/src/Datos.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, {useState} from "react";
 import Resultado from "./resultado";
 
 export default function Datos(){
@@ -10,15 +10,33 @@ export default function Datos(){
 
     const handleChange = e =>
     {
+       
         let data = {};
         data[e.target.name]=e.target.value;
         setDato({...dato,...data})
     }
     const handleClick = e =>
     {
+        let regularDui = /^[0-9]{8}-[0-9]{1}$/;
+        let regularString = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+       
+
         if (Object.keys(dato.sexo).length === 0 || dato.sexo.trim() === '') {
         return;
         }
+         
+        //  if (Object.keys(dato.dui).length === 0 || dato.dui.trim() === '') {
+        //      return;
+        //  }
+        //    else if (!regularDui.test(dato.dui.trim()))
+        //     {return}
+        //      if (Object.keys(dato.name).length === 0 || dato.name.trim() === '') {
+        //          return;
+        //         }
+        //          else if (!regularString.test(dato.name.trim()))
+        //      {return}
+          
+            
             setDatos([...datos, dato])
     }
     
@@ -31,8 +49,8 @@ export default function Datos(){
         <input type="text" placeholder="nombres" name="nombres" onChange={handleChange} />
         <input type="text"  placeholder="apellidos" name="apellidos" onChange={handleChange}/>
         <input type="text"  placeholder="sexo" name="sexo"  onChange={handleChange}/>
-        <input type="text"  placeholder="dui" name="dui" onChange={handleChange}/>
-        <input type="text"  placeholder="fecha nacimiento" name="fechaNacimiento" onChange={handleChange}/>
+        <input type="number"  placeholder="dui(ingresar sin guion)" name="dui" onChange={handleChange}/>
+        <input type="date"  placeholder="fecha nacimiento" name="fechaNacimiento" onChange={handleChange}/>
         <textarea type="text"  placeholder="direccion" name="direccion" onChange={handleChange}/>
         <input type="text"  placeholder="municipio" name="municipio" onChange={handleChange}/>
         <input type="text"  placeholder="departamento" name="departamento" onChange={handleChange}/>
